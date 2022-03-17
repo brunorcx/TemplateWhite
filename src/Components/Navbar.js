ee89c07dc47b2bc1be21b391880683ddd684f323
Bruno RC <brunorcx@hotmail.com>
2022-03-17 14:19:10 -0400

    initial version of mobile navbar; Added individual file for colors in css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,53 @@
 import "../Styles/Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
 const Navbar = (props) => {
+  const [slideMenu, setSlideMenu] = useState(false);
   return (
     <div className={props.transparent ? "transparent" : "totalNav"}>
-      <img className="logo" src="/Logo.png" alt="logo"></img>
-      <ul>
-        <li onClick={() => props.setNavbarClick("Sobre")}>Sobre</li>
-        <li onClick={() => props.setNavbarClick("Planos")}>Planos</li>
-        <li onClick={() => props.setNavbarClick("Modelos")}>Modelos</li>
-        <li onClick={() => props.setNavbarClick("Contatos")}>Contato</li>
-      </ul>
+      <div className="Nav1">
+        <img className="logo" src="/Logo.png" alt="logo"></img>
+        <div className={slideMenu ? "Slide" : "MobHamburger"}>
+          <GiHamburgerMenu onClick={() => setSlideMenu(!slideMenu)} />
+        </div>
+      </div>
+      <div className={slideMenu ? "translateInX" : "Nav2"}>
+        <ul>
+          <li
+            onClick={() => {
+              props.setNavbarClick("Sobre");
+              if (slideMenu) setSlideMenu(!slideMenu); //Check for conflict with PC
+            }}
+          >
+            Sobre
+          </li>
+          <li
+            onClick={() => {
+              props.setNavbarClick("Planos");
+              if (slideMenu) setSlideMenu(!slideMenu);
+            }}
+          >
+            Planos
+          </li>
+          <li
+            onClick={() => {
+              props.setNavbarClick("Modelos");
+              if (slideMenu) setSlideMenu(!slideMenu);
+            }}
+          >
+            Modelos
+          </li>
+          <li
+            onClick={() => {
+              props.setNavbarClick("Contatos");
+              if (slideMenu) setSlideMenu(!slideMenu);
+            }}
+          >
+            Contato
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
